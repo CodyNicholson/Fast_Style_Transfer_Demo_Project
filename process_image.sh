f1d70aea52e8b97258ecd7b77bd010e14c5cc182
Cody Nicholson <codynicholson96@gmail.com>
2017-11-14 10:55:23 -0600

    Updated sh file

--- process_image.sh
+++ process_image.sh
@@ -5,3 +5,4 @@ python evaluate.py --checkpoint wreck.ckpt --in-path in.jpg --out-path out/wreck
 python evaluate.py --checkpoint rain-princess.ckpt --in-path in.jpg --out-path out/rain-princess-out.jpg
 python evaluate.py --checkpoint udnie.ckpt --in-path in.jpg --out-path out/udnie-out.jpg
 python evaluate.py --checkpoint la_muse.ckpt --in-path in.jpg --out-path out/la_muse-out.jpg
+mv in.jpg out/in.jpg
