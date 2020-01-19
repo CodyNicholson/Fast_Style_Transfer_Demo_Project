c1861e502cb76b8c6c6a1a974ffb4c0ce6e2a107
Cody Nicholson <codynicholson96@gmail.com>
2020-01-19 10:21:57 -0600

    readded rain princess style

--- process_image.sh
+++ process_image.sh
@@ -1,5 +1,5 @@
 set -x
-source activate style-transfer
+# source activate style-transfer
 python src/evaluate.py --checkpoint src/styles/scream.ckpt --in-path in.jpg --out-path out/scream-out.jpg
 python src/evaluate.py --checkpoint src/styles/wreck.ckpt --in-path in.jpg --out-path out/wreck-out.jpg
 python src/evaluate.py --checkpoint src/styles/rain-princess.ckpt --in-path in.jpg --out-path out/rain-princess-out.jpg
@@ -7,3 +7,4 @@ python src/evaluate.py --checkpoint src/styles/udnie.ckpt --in-path in.jpg --out
 python src/evaluate.py --checkpoint src/styles/la_muse.ckpt --in-path in.jpg --out-path out/la_muse-out.jpg
 python src/evaluate.py --checkpoint src/styles/wave.ckpt --in-path in.jpg --out-path out/wave-out.jpg
 # mv in.jpg out/in.jpg
+rm in.jpg
